c598a1a6fcec90deda3c6b497d2ae00ffd552d68
chintamani <chintamani@chintamani-PC>
2020-06-05 15:54:50 +0530

    Just commit

--- jbktest/src/main/java/com/jbk/controller/QuestionsController.java
+++ jbktest/src/main/java/com/jbk/controller/QuestionsController.java
@@ -9,15 +9,13 @@ public class QuestionsController {
 	
 	public Question getQuestions() {  // this is a just dumy code...
 		Question question=new Question();
-		question.setQuestionText("java is platform independent");
+		question.setQuestionText("java is platform independent...");
 		question.setOption1("Yes");
 		question.setOption2("No");
 		question.setCorrectoption("1");
 		question.setExplanation("due to byte code...which is os independent");
-		
-
 		return question;
 		
 	}
-
+	
 }
