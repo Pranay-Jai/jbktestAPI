be0ac5965d81923f4daf332d4059fd133a426a8e
chintamani <chintamani@chintamani-PC>
2020-06-06 11:52:24 +0530

    pranay commit

--- jbktest/src/main/java/com/jbk/controller/QuestionsController.java
+++ jbktest/src/main/java/com/jbk/controller/QuestionsController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("testapi")
 public class QuestionsController {
 	
-<<<<<<< HEAD
-	public Question getQuestions() {  // this is a just dumy code...
-=======
+
 	public Question getQuestions() {  // this is a just dumy code
->>>>>>> branch 'master' of https://github.com/Pranay-Jai/jbktestAPI.git
 		Question question=new Question();
 		question.setQuestionText("java is platform independent...");
 		question.setOption1("Yes");
